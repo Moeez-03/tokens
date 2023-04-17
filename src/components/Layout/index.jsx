9b791c3855beac86a3b83c0f9a7d74a5956fdc3f
Moeez-03 <moeez2820@gmail.com>
2023-04-18 03:02:18 +0500

    codoez

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Footer from '../Footer';
+import Navbar from '../Navbar';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className='overflow-hidden'>
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node
+};
